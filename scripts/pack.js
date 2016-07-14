4052b447e1068c63351df44a4835f69d34b9e49e
Ben Williams <ben@smash.mn>
2016-07-14 20:06:45 +0100

    Particle video implementation

--- scripts/pack.js
+++ scripts/pack.js
@@ -1,18 +1,30 @@
-var shade, zoomimg;
-
-function zoomAsset(asset) {
-	zooming.src = asset.firstElementChild.src;
-	shade.fadeIn(200);
-}
+var shade, zoomimg, vidframe;
 
 $(function(){
 	$("body").addClass("js-enabled");
+
 	shade = $("#shade");
-	shade.click(function(){shade.fadeOut(200)});
-	zooming = shade.find("img")[0];
+	zoomimg = shade.find("img")[0];
+
+	shade.click(function(){shade.fadeOut(200);vidframe.remove();});
+
 	$(".asset.model .imgholder").click(function(ev){
-		console.log(ev);
-		ev.stopPropagation();
-		zoomAsset(ev.currentTarget);
+		zoomimg.src = ev.currentTarget.firstElementChild.src;
+		$(zoomimg).show();
+		shade.removeClass("particle");
+		shade.fadeIn(200);
 	});
+
+	$(".asset.particle .imgholder").click(function(ev){
+		ev.preventDefault();
+		ev.stopPropagation();
+
+		var vidid = ev.currentTarget.dataset.vid;
+		vidframe = $("<iframe class=\"video\" src=\"http://www.youtube.com/embed/" + vidid + "?showinfo=0&amp;vq=hd720&amp;rel=0&amp;autohide=1&amp;autoplay=1\" frameborder=\"0\" allowfullscreen=\"\"></iframe>");
+		shade.addClass("particle");
+		shade.children().append(vidframe);
+		$(zoomimg).hide();
+		shade.fadeIn(200);
+	});
+
 });
