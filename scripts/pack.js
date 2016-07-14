20472194ea4277acde103b9590e84df643136484
Ben Williams <ben@smash.mn>
2016-07-14 19:10:31 +0100

    Image zooming for pack

---
+++ scripts/pack.js
@@ -0,0 +1,18 @@
+var shade, zoomimg;
+
+function zoomAsset(asset) {
+	zooming.src = asset.firstElementChild.src;
+	shade.fadeIn(200);
+}
+
+$(function(){
+	$("body").addClass("js-enabled");
+	shade = $("#shade");
+	shade.click(function(){shade.fadeOut(200)});
+	zooming = shade.find("img")[0];
+	$(".imgholder").click(function(ev){
+		console.log(ev);
+		ev.stopPropagation();
+		zoomAsset(ev.currentTarget);
+	});
+});
